6203700df68c1acd50d6a01cc5f9be903e0708d5
eizyc66 <eizyc66@gmail.com>
2023-09-04 16:55:31 +0800

    fix: update

---
+++ _next/static/chunks/pages/[locale]/about/introduction-d221977d1a141d44.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[757],{2308:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[locale]/about/introduction",function(){return _(5819)}])}},function(n){n.O(0,[128,897,888,179],function(){return n(n.s=2308)}),_N_E=n.O()}]);
